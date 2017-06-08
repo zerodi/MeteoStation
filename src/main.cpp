1d7c48e0860450575aa7f34b3f9936ccea2bf0a9
Dmitriy Pyankov <mail@zerodi.ru>
2017-06-08 11:48:52 +0500

    Разделение подключения к wifi и mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,58 @@
+/**
+* MeteoStation
+* Based on esp8266 (ModeMCU v1.0) and BME280
+*/
 #include <Arduino.h>
+
+//Sensor libs
 #include <Wire.h>
+#include <BME280I2C.h>
+
+//Cool stuff for sending json
 #include <ArduinoJson.h>
 
+//Main web libs
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+//Work with mornal time, which we get from ntp server
 #include <TimeLib.h>
 #include <NtpClientLib.h>
 
-#include <BME280I2C.h>
-
-#define SERIAL_BAUD 115200
-
 //WIFI
-const char* ssid = "wifi-ssid";
-const char* pass = "password";
+const char* wifi_ssid = "ssid";
+const char* wifi_password = "password";
 
 //MQTT
-const char* mqtt_server = "xxx.xxx.xxx.xxxx";
+const char* mqtt_server = "x.x.x.x";
 const int mqtt_port = 1883;
 const char* mqtt_user = "user";
-const char* mqtt_pass = "pass";
-void callback(char* topic, byte* payload, unsigned int length) {
-  // handle message arrived
-}
+const char* mqtt_password = "password";
+const char* mqtt_topic = "esp/meteo";
 
-//BME280
-bool metric = true;
-//NTP
-int8_t timeZone = 5;
+
 //Init all
+WiFiClient wificlient;
+PubSubClient client(wificlient);
 BME280I2C bme;
-WiFiClient wclient;
-PubSubClient client(mqtt_server, mqtt_port, callback, wclient);
 
+int8_t timeZone = 5; //Your timeZone
+bool metric = true; //Set your metric system
 long lastSync = 0;
-long syncInterval = 30 * 1000;
-// void getTime() {}
+long syncInterval = 60 * 1000;
 
 float collTemp = 0.0;
 float collHum = 0.0;
 float collPres = 0.0;
 int count = 0;
 
-void connect() {
+void connect_wifi() {
   //Wifi: Inform
-  Serial.println();
   Serial.print("Connecting to ");
-  Serial.print(ssid);
-  Serial.print(".");
+  Serial.print(wifi_ssid);
   //Wifi: init Connecting
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
+  WiFi.begin(wifi_ssid, wifi_password);
   //Wifi: wait until connected
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -63,24 +64,23 @@ void connect() {
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
 
-  //MQTT: Inform
-  Serial.print("Connecting to MQTT server.");
-  //MQTT: init connecting
-  client.connect("espMeteo", mqtt_user, mqtt_pass);
-  while(!client.connected()) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-  Serial.println("Connected to MQTT server");
-
   NTP.begin("pool.ntp.org", timeZone, false);
   NTP.setInterval(63);
 }
 
-// void sensors() {
-//   Serial.println();
-// }
+void connect_mqtt() {
+  while(!client.connected()) {
+    Serial.print("Connecting to MQTT server...");
+    if (client.connect("espMeteo", mqtt_user, mqtt_password)) {
+      Serial.println("\tConnected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
 
 void show(JsonObject &root) {
   Serial.print(root.get<String>("time"));
@@ -92,19 +92,19 @@ void show(JsonObject &root) {
   Serial.print("% RH");
   Serial.print("\t\tPressure: ");
   Serial.print(root.get<String>("pressure"));
-  Serial.println(" torr");
+  Serial.println(" bar");
 }
 
 void publish (JsonObject &root) {
   char jsonChar[100];
   root.printTo(jsonChar);
-  client.publish("bedroom/meteo", jsonChar);
+  client.publish(mqtt_topic, jsonChar);
 }
 
 void getTemp() {
   float temp(NAN), hum(NAN), pres(NAN);
   // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
-  uint8_t pressureUnit(5);
+  uint8_t pressureUnit(4);
   // Parameters: (float& pressure, float& temp, float& humidity, bool celsius = false, uint8_t pressureUnit = 0x0)
   bme.read(pres, temp, hum, metric, pressureUnit);
   collTemp += temp;
@@ -128,16 +128,14 @@ void resetValues() {
 }
 
 void setup() {
-  Serial.begin(SERIAL_BAUD);
+  Serial.begin(115200);
   while (!Serial) {} // Wait
-  //Just for cool output
-  delay(1000);
-  Serial.print("MeteoStation v.1.0.0");
   while (!bme.begin()) {
     Serial.println("Could not find BME280 sensor!");
     delay(1000);
   }
-  connect();
+  connect_wifi();
+  client.setServer(mqtt_server, mqtt_port);
 }
 
 void loop() {
@@ -146,17 +144,12 @@ void loop() {
   //Wifi: check Connection
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("WiFi disconnected");
-    toReconnect = true;
+    connect_wifi();
   }
   if (!client.connected()) {
     Serial.println("MQTT disconnected");
-    toReconnect = true;
+    connect_mqtt();
   }
-
-  if (toReconnect) {
-    connect();
-  }
-
   client.loop();
 
   unsigned long currentMillis = millis();
